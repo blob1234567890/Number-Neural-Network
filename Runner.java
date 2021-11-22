d3c6103bd4859997f6d617814c4a521d57bb9446
blob1234567890 <ben.k.olson@gmail.com>
2021-11-22 10:38:22 -0800

    Runner initializes all training data w/ Reader and forms Neural Network

---
+++ Runner.java
@@ -0,0 +1,20 @@
+public class Runner {
+    public static void main(String[] args) {
+        Reader trainI_reader = new Reader("./Training_Data/train-images-idx3-ubyte", Reader.TYPE_FILE_IDX_IMAGE);
+        Reader trainL_reader = new Reader("./Training_Data/train-labels-idx1-ubyte", Reader.TYPE_FILE_IDX_IMAGE);
+        Reader testI_reader = new Reader("./Testing_Data/t10k-images-idx3-ubyte", Reader.TYPE_FILE_IDX_IMAGE);
+        Reader testL_reader = new Reader("./Testing_Data/t10k-labels-idx1-ubyte", Reader.TYPE_FILE_IDX_IMAGE);
+
+        NeuralNetwork nn = new NeuralNetwork(
+            /**
+             * We are using the MNIST data base so the range of values
+             * in `trainL_reader` is 0-9 (inclusive).
+             * TODO: find range of `trainL_reader` w/out being given
+             * TODO: try different hidden layer numbers and sizes
+             */
+            new int[] {trainI_reader.getImageSize(), 16, 16, 10}
+        );
+
+        
+    }
+}
