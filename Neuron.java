786fb392b368baa65e0730d78c1061307cc5474a
blob1234567890 <ben.k.olson@gmail.com>
2021-11-22 16:43:01 -0800

    Bias, type control, seperate weight init, comments

--- Neuron.java
+++ Neuron.java
@@ -11,28 +11,46 @@ public class Neuron {
 
     private Weight[] forward_weights = null; //all the weights used in forward prop
     private Weight[] back_weights = null; //all the weights used in back prop
+    private double bias;
 
     private int neuron_type;
+    private boolean hasWeights;
 
-    public Neuron(Neuron[] sources, Neuron[] sinks) {
-        neuron_type = TYPE_HIDDEN;
-
-        forward_weights = new Weight[sources.length];
-        back_weights = new Weight[sinks.length];
-
-        //randomize the weights where this is the source
-        for (int i = 0; i < back_weights.length; i++) {
-            back_weights[i] = new Weight(this, sinks[i]);
+    public Neuron(int type) throws IllegalArgumentException{
+        if (type == TYPE_INPUT || type == TYPE_HIDDEN || type == TYPE_OUTPUT) {
+            neuron_type = type;
+        } else {
+            throw new IllegalArgumentException("Neuron type not found");
         }
-
-        for (int i = 0; i < forward_weights.length; i++) {
-            forward_weights[i] = sources[i].getSink(i);
-        }
+        bias = Math.random() * 8.0 - 4.0;
+        hasWeights = false;
     }
 
-    public Neuron(Neuron[] others, int type) {
-        neuron_type = type;
+    //since every Neuron must be connected to a Neuron, you can't create the first one without a Neuron existing, which is an infinite loop
+    //setWeights must always be called before using a Neuron
+    public void setWeights(Neuron[] sources, Neuron[] sinks){
+        //You CAN give the wrong params for the neuron type but I will just do whatever is logical (inputs get sources, outputs get sinks)
+        if (neuron_type == TYPE_HIDDEN) {
+            forward_weights = new Weight[sources.length];
+            back_weights = new Weight[sinks.length];
 
+            //make a new weight when this is the source
+            for (int i = 0; i < back_weights.length; i++) {
+                back_weights[i] = new Weight(this, sinks[i]);
+            }
+
+            for (int i = 0; i < forward_weights.length; i++) {
+                forward_weights[i] = sources[i].getSink(i);
+            }
+        } else if (neuron_type == TYPE_INPUT) {
+            setWeights(sources);
+        } else if (neuron_type == TYPE_OUTPUT) {
+            setWeights(sinks);
+        }
+        hasWeights = true;
+    }
+
+    public void setWeights(Neuron[] others) {
         if (neuron_type == TYPE_INPUT) {
             back_weights = new Weight[others.length];
             for (int i = 0; i < back_weights.length; i++) {
@@ -44,9 +62,14 @@ public class Neuron {
                 forward_weights[i] = others[i].getSink(i);
             }
         }
+        hasWeights = true;
     }
 
     public Weight getSink(int i) {
         return forward_weights[i];
+    }
+
+    public int getType() {
+        return neuron_type;
     }
 }
